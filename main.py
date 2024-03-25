b7de4a5ff0bd551251be7fa42ac8a99e99e9dd72
asiclouduz <164859282+asiclouduz@users.noreply.github.com>
2024-03-25 13:37:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,354 @@
+import argparse
+import traceback
+import asyncio
+import google.generativeai as genai
+import re
+import telebot
+from telebot.async_telebot import AsyncTeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import  Message
+
+admin_url = "https://t.me/itstimebeyond"
+site_url = "https://asicloud.uz"
+tgchannel_url = "https://t.me/storiescreation"
+
+
+generation_config = {
+    "temperature": 0.1,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {   "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    },
+]
+
+error_info="⚠️⚠️⚠️\nNimadir xato ketdi !\nAdminga bog'laning! /admin"
+before_generate_info="🤖Javob qidiryapman🤖"
+download_pic_notify="🤖Rasm taxmin qilinmoqda🤖"
+
+def find_all_index(str, pattern):
+    index_list = [0]
+    for match in re.finditer(pattern, str, re.MULTILINE):
+        if match.group(1) != None:
+            start = match.start(1)
+            end = match.end(1)
+            index_list += [start, end]
+    index_list.append(len(str))
+    return index_list
+
+
+def replace_all(text, pattern, function):
+    poslist = [0]
+    strlist = []
+    originstr = []
+    poslist = find_all_index(text, pattern)
+    for i in range(1, len(poslist[:-1]), 2):
+        start, end = poslist[i : i + 2]
+        strlist.append(function(text[start:end]))
+    for i in range(0, len(poslist), 2):
+        j, k = poslist[i : i + 2]
+        originstr.append(text[j:k])
+    if len(strlist) < len(originstr):
+        strlist.append("")
+    else:
+        originstr.append("")
+    new_list = [item for pair in zip(originstr, strlist) for item in pair]
+    return "".join(new_list)
+
+def escapeshape(text):
+    return "▎*" + text.split()[1] + "*"
+
+def escapeminus(text):
+    return "\\" + text
+
+def escapebackquote(text):
+    return r"\`\`"
+
+def escapeplus(text):
+    return "\\" + text
+
+def escape(text, flag=0):
+    # In all other places characters
+    # _ * [ ] ( ) ~ ` > # + - = | { } . !
+    # must be escaped with the preceding character '\'.
+    text = re.sub(r"\\\[", "@->@", text)
+    text = re.sub(r"\\\]", "@<-@", text)
+    text = re.sub(r"\\\(", "@-->@", text)
+    text = re.sub(r"\\\)", "@<--@", text)
+    if flag:
+        text = re.sub(r"\\\\", "@@@", text)
+    text = re.sub(r"\\", r"\\\\", text)
+    if flag:
+        text = re.sub(r"\@{3}", r"\\\\", text)
+    text = re.sub(r"_", "\_", text)
+    text = re.sub(r"\*{2}(.*?)\*{2}", "@@@\\1@@@", text)
+    text = re.sub(r"\n{1,2}\*\s", "\n\n• ", text)
+    text = re.sub(r"\*", "\*", text)
+    text = re.sub(r"\@{3}(.*?)\@{3}", "*\\1*", text)
+    text = re.sub(r"\!?\[(.*?)\]\((.*?)\)", "@@@\\1@@@^^^\\2^^^", text)
+    text = re.sub(r"\[", "\[", text)
+    text = re.sub(r"\]", "\]", text)
+    text = re.sub(r"\(", "\(", text)
+    text = re.sub(r"\)", "\)", text)
+    text = re.sub(r"\@\-\>\@", "\[", text)
+    text = re.sub(r"\@\<\-\@", "\]", text)
+    text = re.sub(r"\@\-\-\>\@", "\(", text)
+    text = re.sub(r"\@\<\-\-\@", "\)", text)
+    text = re.sub(r"\@{3}(.*?)\@{3}\^{3}(.*?)\^{3}", "[\\1](\\2)", text)
+    text = re.sub(r"~", "\~", text)
+    text = re.sub(r">", "\>", text)
+    text = replace_all(text, r"(^#+\s.+?$)|```[\D\d\s]+?```", escapeshape)
+    text = re.sub(r"#", "\#", text)
+    text = replace_all(
+        text, r"(\+)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeplus
+    )
+    text = re.sub(r"\n{1,2}(\s*)-\s", "\n\n\\1• ", text)
+    text = re.sub(r"\n{1,2}(\s*\d{1,2}\.\s)", "\n\n\\1", text)
+    text = replace_all(
+        text, r"(-)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeminus
+    )
+    text = re.sub(r"```([\D\d\s]+?)```", "@@@\\1@@@", text)
+    text = replace_all(text, r"(``)", escapebackquote)
+    text = re.sub(r"\@{3}([\D\d\s]+?)\@{3}", "```\\1```", text)
+    text = re.sub(r"=", "\=", text)
+    text = re.sub(r"\|", "\|", text)
+    text = re.sub(r"{", "\{", text)
+    text = re.sub(r"}", "\}", text)
+    text = re.sub(r"\.", "\.", text)
+    text = re.sub(r"!", "\!", text)
+    return text
+
+# Prevent "create_convo" function from blocking the event loop.
+async def make_new_gemini_convo():
+    loop = asyncio.get_running_loop()
+
+    def create_convo():
+        model = genai.GenerativeModel(
+            model_name="gemini-pro",
+            generation_config=generation_config,
+            safety_settings=safety_settings,
+        )
+        convo = model.start_chat()
+        return convo
+
+    # Run the synchronous "create_convo" function in a thread pool
+    convo = await loop.run_in_executor(None, create_convo)
+    return convo
+
+# Prevent "send_message" function from blocking the event loop.
+async def send_message(player, message):
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, player.send_message, message)
+    
+# Prevent "model.generate_content" function from blocking the event loop.
+async def async_generate_content(model, contents):
+    loop = asyncio.get_running_loop()
+
+    def generate():
+        return model.generate_content(contents=contents)
+
+    response = await loop.run_in_executor(None, generate)
+    return response
+
+async def main():
+    # Init args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tg_token", help="TG API")
+    parser.add_argument("GOOGLE_GEMINI_KEY", help="Geimini api")
+    options = parser.parse_args()
+    print("Arg parse done.")
+    gemini_player_dict = {}
+
+    genai.configure(api_key=options.GOOGLE_GEMINI_KEY)
+
+    # Init bot
+    bot = AsyncTeleBot(options.tg_token)
+    await bot.delete_my_commands(scope=None, language_code=None)
+    await bot.set_my_commands(
+        commands=[
+            telebot.types.BotCommand("start", "🚀Boshlash"),
+            telebot.types.BotCommand("profile", "💎Hisobim"),
+            telebot.types.BotCommand("additional", "🌐Qo'shimcha"),
+            telebot.types.BotCommand("admin", "📶Admin"),
+            telebot.types.BotCommand("gemini", "👥Bu buyruq faqat guruhlar uchun!"),
+            telebot.types.BotCommand("clear", "🧹Tarixni tozalash")
+        ],
+    )
+    print("Bot init done.")
+
+    # Init commands
+    @bot.message_handler(commands=["start"])
+    async def gemini_handler(message: Message):
+        try:
+            await bot.reply_to( message , escape("Salom Gemini botimga hush kelibsiz!. Bot Google Gemini AI tomonidan qo'llab quvvatlanadi\nSavolingizni berishingiz mumkin"), parse_mode="MarkdownV2")
+        except IndexError:
+            await bot.reply_to(message, error_info)
+            
+   
+     
+    @bot.message_handler(commands=["admin"])
+    async def admin_command(message: Message):
+    # Create inline keyboard
+      keyboard = InlineKeyboardMarkup()
+      keyboard.add(InlineKeyboardButton("Adminga yozish", url=admin_url))
+    
+    # Send message with inline keyboard
+      await bot.send_message(message.chat.id, "Pastdagi tugmani bosing:", reply_markup=keyboard)
+      
+    @bot.message_handler(commands=["additional"])
+    async def additional_command(message: Message):
+    # Create inline keyboard
+      keyboard = InlineKeyboardMarkup()
+      keyboard.add(InlineKeyboardButton("Websayt", url=site_url))
+      keyboard.add(InlineKeyboardButton("Telegram stories", url=tgchannel_url))
+    
+    # Send message with inline keyboard
+      await bot.send_message(message.chat.id, "Qo'shimcha havolalar:", reply_markup=keyboard)
+      
+    @bot.message_handler(commands=["profile"])
+    async def profile_command(message: Message):
+    # Retrieve user's ID and name
+     user_id = message.from_user.id
+     user_name = message.from_user.first_name
+    
+    # Compose profile message
+     profile_message = f"👤 *Hisobingiz*\n\nID: `{user_id}`\nIsm: {user_name}"
+    
+    # Send profile message
+     await bot.send_message(message.chat.id, profile_message, parse_mode="MarkdownV2")
+
+
+
+    @bot.message_handler(commands=["gemini"])
+    async def gemini_handler(message: Message):
+
+        if message.chat.type == "private":
+            await bot.reply_to( message , "Bu faqat guruhlar uchun ishlaydi!")
+            return
+        try:
+            m = message.text.strip().split(maxsplit=1)[1].strip()
+        except IndexError:
+            await bot.reply_to( message , escape("Iltimos so'rovingizni /gemini buyrug'idan so'ng yozishni unutmang! \nMisol uchun: `/gemini AsiCloud nima?` yoki `/gemini Menga yordam ber`"), parse_mode="MarkdownV2")
+            return
+        player = None
+        if str(message.from_user.id) not in gemini_player_dict:
+            player = await make_new_gemini_convo()
+            gemini_player_dict[str(message.from_user.id)] = player
+        else:
+            player = gemini_player_dict[str(message.from_user.id)]
+        if len(player.history) > 10:
+            player.history = player.history[2:]
+        try:
+            sent_message = await bot.reply_to(message, before_generate_info)
+            await send_message(player, m)
+            try:
+                await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id, parse_mode="MarkdownV2")
+            except:
+                await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+
+        except Exception:
+            traceback.print_exc()
+            await bot.edit_message_text(error_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+
+    @bot.message_handler(commands=["clear"])
+    async def gemini_handler(message: Message):
+        # Check if the player is already in gemini_player_dict.
+        if str(message.from_user.id) in gemini_player_dict:
+            del gemini_player_dict[str(message.from_user.id)]
+            await bot.reply_to(message, "Tarix tozalandi!")
+        else:
+            await bot.reply_to(message, "Sizda tarix mavjud emas!")
+    
+    @bot.message_handler(func=lambda message: message.chat.type == "private", content_types=['text'])
+    async def gemini_private_handler(message: Message):
+        m = message.text.strip()
+        player = None 
+        # Check if the player is already in gemini_player_dict.
+        if str(message.from_user.id) not in gemini_player_dict:
+            player = await make_new_gemini_convo()
+            gemini_player_dict[str(message.from_user.id)] = player
+        else:
+            player = gemini_player_dict[str(message.from_user.id)]
+        # Control the length of the history record.
+        if len(player.history) > 10:
+            player.history = player.history[2:]
+        try:
+            sent_message = await bot.reply_to(message, before_generate_info)
+            await send_message(player, m)
+            try:
+                await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id, parse_mode="MarkdownV2")
+            except:
+                await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+
+        except Exception:
+            traceback.print_exc()
+            await bot.reply_to(message, error_info)
+
+    @bot.message_handler(content_types=["photo"])
+    async def gemini_photo_handler(message: Message) -> None:
+        if message.chat.type != "private":
+            s = message.caption
+            if not s or not (s.startswith("/gemini")):
+                return
+            try:
+                prompt = s.strip().split(maxsplit=1)[1].strip() if len(s.strip().split(maxsplit=1)) > 1 else ""
+                file_path = await bot.get_file(message.photo[-1].file_id)
+                sent_message = await bot.reply_to(message, download_pic_notify)
+                downloaded_file = await bot.download_file(file_path.file_path)
+            except Exception:
+                traceback.print_exc()
+                await bot.reply_to(message, error_info)
+            model = genai.GenerativeModel("gemini-pro-vision")
+            contents = {
+                "parts": [{"mime_type": "image/jpeg", "data": downloaded_file}, {"text": prompt}]
+            }
+            try:
+                await bot.edit_message_text(before_generate_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+                response = await async_generate_content(model, contents)
+                await bot.edit_message_text(response.text, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+            except Exception:
+                traceback.print_exc()
+                await bot.edit_message_text(error_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+        else:
+            s = message.caption if message.caption else ""
+            try:
+                prompt = s.strip()
+                file_path = await bot.get_file(message.photo[-1].file_id)
+                sent_message = await bot.reply_to(message, download_pic_notify)
+                downloaded_file = await bot.download_file(file_path.file_path)
+            except Exception:
+                traceback.print_exc()
+                await bot.reply_to(message, error_info)
+            model = genai.GenerativeModel("gemini-pro-vision")
+            contents = {
+                "parts": [{"mime_type": "image/jpeg", "data": downloaded_file}, {"text": prompt}]
+            }
+            try:
+                await bot.edit_message_text(before_generate_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+                response = await async_generate_content(model, contents)
+                await bot.edit_message_text(response.text, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+            except Exception:
+                traceback.print_exc()
+                await bot.edit_message_text(error_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+    # Start bot
+    print("Starting Gemini_Telegram_Bot.")
+    await bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    asyncio.run(main())
